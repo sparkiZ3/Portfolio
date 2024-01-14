9fafbbf78ac9497d72effe819869cf10ba4c2c4a
sparkiZ3 <noan.lissillour@gmail.com>
2024-01-14 18:01:32 +0100

    few more translations

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-const id =['main_presentation','contactbuttonmain']
+const id =['main_presentation','contactbuttonmain','mona_ozouf','mona_ozouf_text']
 const translation = {
     fr: {
-        main_presentation: "<p id=\"main_presentation\">Hey ! je suis Noan et je suis un étudiant dévelopeur. J'étudie actuellement la programmation dans le cadre de mes etudes a l'IUT.</p>",
-        contactbuttonmain :"<a id=\"contactbuttonmain\" href=\"#\">Me contacter</p><div class=\"contactbutton\"></div></a>"
+        main_presentation: "Hey ! je suis Noan et je suis un étudiant dévelopeur. J'étudie actuellement la programmation dans le cadre de mes etudes a l'IUT.",
+        contactbuttonmain :"Me contacter<div class=\"contactbutton\"></div>",
+        mona_ozouf :"College Mona Ozouf",
+        mona_ozouf_text :"C'est a partir du collège ou je decouvre le développement via scratch. Puis par la suite je me suis mit a apprendre le python. Mon stage de 3eme se portant sur le milieu de l'informatique, j'ai pu y rencontrer des developpeurs ce qui a confirmé mon choix de devenir developpeur",
     },
     en: {
-        main_presentation: "<p id=\"main_presentation\">Hey !  I'm Noan and i'm a student developer. I'm actually learning the programation at the IUT of Nantes</p>",
-        contactbuttonmain :"<a id=\"contactbuttonmain\" href=\"#\">Contact Me</p><div class=\"contactbutton\"></div></a>"
+        main_presentation: "Hey !  I'm Noan and i'm a student developer. I'm actually learning the programation at the IUT of Nantes",
+        contactbuttonmain :"Contact Me<div class=\"contactbutton\"></div>",
+        mona_ozouf :"Mona Ozouf high school",
+        mona_ozouf_text :"C'est a partir du collège where i discovered developement via scratch. Then, i started learning python. Mon stage de 3eme se portant sur le milieu de l'informatique, j'ai pu y rencontrer des developpeurs ce qui a confirmé mon choix de devenir developpeur",
     },
 };
 function chgLang(){
@@ -18,7 +22,7 @@ function chgLang(){
             iddata=id[i]
             document.getElementById(iddata).innerHTML=translation.en[iddata];
         }
-    }else if(langPath==currentLoc+"imgs/en.png"){//on change la langue en francais
+    }else if(langPath==currentLoc+"imgs/en.png"){//on change la langue en français
         document.getElementById("lang").src=currentLoc+"imgs/fr.png";
         for (i=0;i<id.length;i++){
             iddata=id[i]
